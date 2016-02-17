a711e5526bbdf7e5ab72599776f2557e46a8311b
Mariem Rabia <mariem.rabia@esprit.tn>
2016-02-17 19:27:20 +0100

    Signed-off-by: Mariem Rabia <mariem.rabia@esprit.tn>

--- ideca2/ideca2-client/src/main/java/pdev/financialbrains/client/boot/Main.java
+++ ideca2/ideca2-client/src/main/java/pdev/financialbrains/client/boot/Main.java
@@ -1,20 +1,27 @@
 package pdev.financialbrains.client.boot;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
 
 import pdev.financialbrains.ejb.contracts.IStockCrudServicesRemote;
 import pdev.financialbrains.ejb.entities.Stock;
+import pdev.financialbrains.ejb.entities.User;
 
 public class Main {
 
-	public static void main(String[] args) {
-		try{
-			Context context = new InitialContext(); 
-			IStockCrudServicesRemote proxy = (IStockCrudServicesRemote) context.lookup("/ideca2-ejb/StockCrudServices!pdev.financialbrains.ejb.contracts.IStockCrudServicesRemote");
+	public static void main(String[] args) throws Exception {
+		//try{
+		//	Context context = new InitialContext(); 
+			//IStockCrudServicesRemote proxy = (IStockCrudServicesRemote) context.lookup("/ideca2-ejb/StockCrudServices!pdev.financialbrains.ejb.contracts.IStockCrudServicesRemote");
 			
-			Stock stock = new Stock(); 
+			//Stock stock = new Stock(); 
 			
 //			stock.setId(1);	
 //			stock1.setCompanyName("Toshiba");
@@ -25,12 +32,26 @@ public class Main {
 			
 //			proxy.delete(stock);
 			
-			stock = proxy.readById(2);
+		/*	stock = proxy.readById(2);
 			
 			System.out.print(stock);
 			
 		} catch(NamingException e){
 			e.printStackTrace();
 		}
-	}
-}
+	}*/
+		/*String uri =
+			    "https://feeds.finance.yahoo.com/rss/2.0/headline?s=yhoo&region=US&lang=en-US";
+			URL url = new URL(uri);
+			HttpURLConnection connection =
+			    (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Accept", "application/xml");
+
+			JAXBContext jc = JAXBContext.newInstance(User.class);
+			InputStream xml = connection.getInputStream();
+		User customer =
+			    (User) jc.createUnmarshaller().unmarshal(xml);
+
+			connection.disconnect();*/
+}}
