32a1613c480cada71d46597dacdd17a7a5e96f0f
MarwaBj <MarwaBj@192.168.1.4>
2016-04-10 13:16:20 +0100

    pages Style
    Signed-off-by: MarwaBj <MarwaBj@192.168.1.4>

--- ideca2/ideca2-ejb/src/main/java/pdev/financialbrains/ejb/contracts/IFxoptionCrudServicesLocal.java
+++ ideca2/ideca2-ejb/src/main/java/pdev/financialbrains/ejb/contracts/IFxoptionCrudServicesLocal.java
@@ -10,16 +10,20 @@ import pdev.financialbrains.ejb.entities.FixedIncomeSecuritie;
 
 @Local
 public interface IFxoptionCrudServicesLocal {
-<<<<<<< HEAD
+
 	void create(ForeignExchangeOption fxoption);
+
 	void update(ForeignExchangeOption fxoption);
+
 	void delete(ForeignExchangeOption fxoption);
+
 	ForeignExchangeOption read(Integer id);
+
 	List<ForeignExchangeOption> readAll();
-	Double Pricing(Integer callPut , Integer currentSpotPrice, Integer dRisk,Integer fRisk,Integer time,Float strikePrice,Float volatilite);
-	 double N(double z);
-=======
 
+	Double Pricing(Integer callPut, Integer currentSpotPrice, Integer dRisk, Integer fRisk, Integer time,
+			Float strikePrice, Float volatilite);
 
->>>>>>> refs/remotes/origin/master
+	double N(double z);
+
 }
