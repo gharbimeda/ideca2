cda41542a8e517af3370b36168ca89494f7fdc89
Ghada <ghada.drissi@esprit.tn>
2016-04-10 01:31:31 +0100

    work

--- ideca2/ideca2-ejb/src/main/java/pdev/financialbrains/ejb/contracts/IUserCrudServiceLocal.java
+++ ideca2/ideca2-ejb/src/main/java/pdev/financialbrains/ejb/contracts/IUserCrudServiceLocal.java
@@ -9,17 +9,13 @@ import pdev.financialbrains.ejb.entities.User;
 @Local
 public interface IUserCrudServiceLocal {
 	
-<<<<<<< HEAD
+	List<User> readAllButOne(Integer id);
 	User authentification(String login, String password);
 
 	List<User> readAll(Integer id);
 
 	List<User> read();
-=======
-	List<User> readAllButOne(Integer id);
-	
 	User readById(Integer id);
->>>>>>> refs/remotes/origin/master
 
 	List<User> readAll(Integer id);
 	
