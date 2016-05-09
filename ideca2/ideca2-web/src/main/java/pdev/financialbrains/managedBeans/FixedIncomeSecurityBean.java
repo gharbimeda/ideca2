fe69ebb49aa639ba32fc23fdff649cf4e4349426
Portable DELL <Portable DELL@172.16.178.41>
2016-05-09 22:29:07 +0200

    SOME CHANGES Signed-off-by: Portable DELL <Portable DELL@172.16.178.41>

--- ideca2/ideca2-web/src/main/java/pdev/financialbrains/managedBeans/FixedIncomeSecurityBean.java
+++ ideca2/ideca2-web/src/main/java/pdev/financialbrains/managedBeans/FixedIncomeSecurityBean.java
@@ -1,5 +1,6 @@
 package pdev.financialbrains.managedBeans;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -9,6 +10,11 @@ import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import pdev.financialbrains.ejb.contracts.IFixedIncomeSecurityLocal;
 import pdev.financialbrains.ejb.entities.Bond;
@@ -30,6 +36,8 @@ public class FixedIncomeSecurityBean {
 	@EJB
 	TradeCrudServices tradeService;
 
+	public static List<Float> BondYieldToday;
+	
 	@EJB
 	derivativesCrudService derivativesCrudService;
 	
@@ -54,7 +62,7 @@ private String curYield;
 	private Float faceValue;
 	private int months;
     private String maturity;
-	private Float couponRate;
+	private Float couponRate=0f;
 	private Float yield;
 	private Float currentYield;
 	private Float bondPrice;
@@ -141,6 +149,7 @@ private String curYield;
 	public void init() {
 		fixedIncomeSecurities = fxlocal.readAll();
 		fixes = remplir();
+		BondYieldToday =list_bond_yield_today();
 
 	}
 
@@ -365,55 +374,57 @@ private String curYield;
 	}
 	public void handleChange(){  
 		System.out.println(maturity);
-	    if(timeMaturity.equals("3 Month"))
+		System.out.println(Yield1Today());
+		System.out.println(couponRate);
+	    if(maturity.equals("3 Month"))
 	    {
 	    	timeMaturity=0.25f;
-	    	currentYield=Bond2Bean.Yield1Today();
-	    	yield=Bond2Bean.Yield1Today();
-	    	System.out.println(currentYield);
+	    	couponRate=Yield1Today();
+	    	yield=Yield1Today();
+	    	System.out.println(couponRate);
 	    }
 	    else if(maturity.equals("6 Month"))
 	    {
 	    	timeMaturity=0.5f;
-	    	currentYield=Bond2Bean.Yield2Today();
-	    	yield=Bond2Bean.Yield2Today();
-	    	System.out.println(currentYield);
+	    	couponRate=Yield2Today();
+	    	yield=Yield2Today();
+	    	System.out.println(couponRate);
 	    }
 	    else if(maturity.equals("2 Year"))
 	    {
 	    	timeMaturity=2f;
-	    	currentYield=Bond2Bean.Yield3Today();
-	    	yield=Bond2Bean.Yield3Today();
-	    	System.out.println(currentYield);
+	    	couponRate=Yield3Today();
+	    	yield=Yield3Today();
+	    	System.out.println(couponRate);
 	    }
 	    else if(maturity.equals("3 Year"))
 	    {
 	    	timeMaturity=3f;
-	    	currentYield=Bond2Bean.Yield4Today();
-	    	yield=Bond2Bean.Yield4Today();
-	    	System.out.println(currentYield);
+	    	couponRate=Yield4Today();
+	    	yield=Yield4Today();
+	    	System.out.println(couponRate);
 	    }
 	    else if(maturity.equals("5 Year"))
 	    {
 	    	timeMaturity=5f;
-	    	currentYield=Bond2Bean.Yield5Today();
-	    	yield=Bond2Bean.Yield5Today();
-	    	System.out.println(currentYield);
+	    	couponRate=Yield5Today();
+	    	yield=Yield5Today();
+	    	System.out.println(couponRate);
 	    	
 	    }
 	    else if(maturity.equals("10 Year"))
 	    {
 	    	timeMaturity=10f;
-	    	currentYield=Bond2Bean.Yield6Today();
-	    	yield=Bond2Bean.Yield6Today();
-	    	System.out.println(currentYield);
+	    	couponRate=Yield6Today();
+	    	yield=Yield6Today();
+	    	System.out.println(couponRate);
 	    }
 	    else if(maturity.equals("30 Year"))
 	    {
 	    	timeMaturity=30f;
-	    	currentYield=Bond2Bean.Yield7Today();
-	    	yield=Bond2Bean.Yield7Today();
-	    	System.out.println(currentYield);
+	    	couponRate=Yield7Today();
+	    	yield=Yield7Today();
+	    	System.out.println(couponRate);
 	    }
 	}
 	public List<String> list_maturity()
@@ -436,4 +447,224 @@ private String curYield;
 	public void setMaturity(String maturity) {
 		this.maturity = maturity;
 	}
+	public List<Float> list_bond_yield_today(){
+		List<String> yield = new ArrayList<>();
+		List<String> bondYield = new ArrayList<>();
+		List<Float> bondYieldFinal = new ArrayList<>();
+	      String html = "http://finance.yahoo.com/bonds";
+	        try {
+	           Document doc = Jsoup.connect(html).get();
+	           Elements tableElements = doc.select("table");
+	           Elements tableHeaderEles = tableElements.select("thead tr th");
+	          
+	           for (int i = 0; i < tableHeaderEles.size(); i++) {
+	              
+	           }
+	           
+	           Elements tableRowElements = tableElements.select(":not(thead) tr");
+	           for (int i = 1; i < tableRowElements.size()-2; i++) {
+	              Element row = tableRowElements.get(i);
+	              Elements rowItems = row.select("td");
+	              for (int j = 0; j < rowItems.size(); j++) {
+	            	  yield.add(rowItems.get(j).text());
+	                 
+	              }
+	           }
+	        } catch (IOException e) {
+	           e.printStackTrace();
+	        }
+	        
+	        
+	        for (int i = 0 ; i<yield.size();i++)
+	        {
+	        }
+	        int i = 1 ;
+	        		while(i<=yield.size())
+	        				{
+	        				bondYield.add(yield.get(i));
+	        					i=i+5;
+	        				}
+	        		int j = 2 ;
+	        		while(j<=yield.size())
+	        		{
+	        			        bondYield.add(yield.get(j));
+	        			        j=j+5;
+	        		}
+	        		int k = 3 ;
+	        		while(k<=yield.size())
+	        		{
+	        			        bondYield.add(yield.get(k));
+	        			        k=k+5;
+	        		}
+	        		int z = 4 ;
+	        		while(z<=yield.size())
+	        		{
+	        			        bondYield.add(yield.get(z));
+	        			        z=z+5;
+	        		}
+					for (String s : bondYield)
+					{
+						Float f = Float.parseFloat(s);
+						bondYieldFinal.add(f);
+					}
+		return bondYieldFinal;
+	}
+	
+
+	
+	public static Float Yield1Today()
+	{
+		return BondYieldToday.get(0);
+	}
+	public static Float Yield2Today()
+	{
+		return BondYieldToday.get(1);
+	}
+	public static Float Yield3Today()
+	{
+		return BondYieldToday.get(2);
+	}
+	public  static Float Yield4Today()
+	{
+		return BondYieldToday.get(3);
+	}
+	public static Float Yield5Today()
+	{
+		return BondYieldToday.get(4);
+	}
+	public  static Float Yield6Today()
+	{
+		return BondYieldToday.get(5);
+	}
+	public static Float Yield7Today()
+	{
+		return BondYieldToday.get(6);
+	}
+	public static Float Yield1Yesterday()
+	{
+		return BondYieldToday.get(7);
+	}
+	public static  Float Yield2Yesterday()
+	{
+		return BondYieldToday.get(8);
+	}
+	public  static Float Yield3Yesterday()
+	{
+		return BondYieldToday.get(9);
+	}
+	public static  Float Yield4Yesterday()
+	{
+		return BondYieldToday.get(10);
+	}
+	public static Float Yield5Yesterday()
+	{
+		return BondYieldToday.get(11);
+	}
+	public  static Float Yield6Yesterday()
+	{
+		return BondYieldToday.get(12);
+	}
+	public static Float Yield7Yesterday()
+	{
+		return BondYieldToday.get(13);
+	}
+	public static Float Yield1LastWeek()
+	{
+		return BondYieldToday.get(14);
+	}
+	public static Float Yield2LastWeek()
+	{
+		return BondYieldToday.get(15);
+	}
+	public static Float Yield3LastWeek()
+	{
+		return BondYieldToday.get(16);
+	}
+	public static Float Yield4LastWeek()
+	{
+		return BondYieldToday.get(17);
+	}
+	public static Float Yield5LastWeek()
+	{
+		return BondYieldToday.get(18);
+	}
+	public  static Float Yield6LastWeek()
+	{
+		return BondYieldToday.get(19);
+	}
+	public static Float Yield7LastWeek()
+	{
+		return BondYieldToday.get(20);
+	}
+	public static Float Yield1LastMonth()
+	{
+		return BondYieldToday.get(21);
+	}
+	public static Float Yield2LastMonth()
+	{
+		return BondYieldToday.get(22);
+	}
+	public static Float Yield3LastMonth()
+	{
+		return BondYieldToday.get(23);
+	}
+	public static Float Yield4LastMonth()
+	{
+		return BondYieldToday.get(24);
+	}
+	public static Float Yield5LastMonth()
+	{
+		return BondYieldToday.get(25);
+	}
+	public static Float Yield6LastMonth()
+	{
+		return BondYieldToday.get(26);
+	}
+	public static Float Yield7LastMonth()
+	{
+		return BondYieldToday.get(27);
+	}
+
+	public TradeCrudServices getTradeService() {
+		return tradeService;
+	}
+
+	public void setTradeService(TradeCrudServices tradeService) {
+		this.tradeService = tradeService;
+	}
+
+	public static List<Float> getBondYieldToday() {
+		return BondYieldToday;
+	}
+
+	public static void setBondYieldToday(List<Float> bondYieldToday) {
+		BondYieldToday = bondYieldToday;
+	}
+
+	public derivativesCrudService getDerivativesCrudService() {
+		return derivativesCrudService;
+	}
+
+	public void setDerivativesCrudService(
+			derivativesCrudService derivativesCrudService) {
+		this.derivativesCrudService = derivativesCrudService;
+	}
+
+	public UserCrudServices getUserCrudServices() {
+		return userCrudServices;
+	}
+
+	public void setUserCrudServices(UserCrudServices userCrudServices) {
+		this.userCrudServices = userCrudServices;
+	}
+
+	public String getCurYield() {
+		return curYield;
+	}
+
+	public void setCurYield(String curYield) {
+		this.curYield = curYield;
+	}
+
+
 }
